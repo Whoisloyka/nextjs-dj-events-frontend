24b18a19580eb2af914417471bb5a08339cd3cdc
Whoisloyka <whoisloyka@gmail.com>
2023-05-03 14:08:41 +0300

    Pages and routing

---
+++ pages/events/[slug].js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function EventPage() {
+  return (
+    <div>
+      <h1>My Event</h1>
+    </div>
+  )
+}
---
+++ pages/events/add.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function AddEventPage() {
+  return (
+    <div>
+      <h1>Add Events Page</h1>
+    </div>
+  )
+}
