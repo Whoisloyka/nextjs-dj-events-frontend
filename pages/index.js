0696a3a887e73cbac950d3fd19e861916dfc9f78
Whoisloyka <whoisloyka@gmail.com>
2023-05-10 17:39:46 +0300

    getServerSideProps & getStaticProps

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,20 @@
 import Layout from "@/components/Layout"
+import { API_URL } from "@/config/index"
 
-export default function Home() {
+export default function Home({ events }) {
   return (
     <Layout>
-      <h1>Home</h1>
+      <h1>Upcoming Events</h1>
     </Layout>
   )
+}
+
+export async function getServerSideProps() {
+  const res = await fetch(`${API_URL}/api/events`)
+  const events = await res.json()
+
+  return {
+    props: { events },
+    revalidate: 1,
+  }
 }
